699ea385c29e8aba16f3cbd749ca1d3af4cf1ff5
Alby-On <66146586+Alby-On@users.noreply.github.com>
2026-03-10 22:46:40 -0300

    Update supabase-client.js

--- gestor-nutricion-catalogo/supabase-client.js
+++ gestor-nutricion-catalogo/supabase-client.js
@@ -1,6 +1,6 @@
 // supabase-client.js
-const SUPABASE_URL = 'https://afrfaeouzkjdkkqeozgq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcmZhZW91emtqZGtrcWVvemdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTg1OTUsImV4cCI6MjA4Nzc5NDU5NX0.CRUaz7sNOuotsV3tVM5O2KvTerAT6uTXHaTy4yKKAdM';
+const SUPABASE_URL = 'https://bochbeuqiruhkhpxdatb.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvY2hiZXVxaXJ1aGtocHhkYXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNDUxNTMsImV4cCI6MjA4ODcyMTE1M30.LncnnP7KUzMv7MVc7TsSq-mepjdYxXxD-8jAYfCBWBo';
 
 // Forzamos que la instancia sea GLOBAL
 window._supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
